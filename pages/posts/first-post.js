0047422a352a3b81f28344aa66bdfafcfe53e231
Odelya <okrief@lightricks.com>
2022-01-18 17:47:32 +0200

    Use getStaticProps to load posts title and name

---
+++ pages/posts/first-post.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout"
+export default function FirstPost() {
+  return (
+    <>
+      <Layout>
+          <div>
+            <Head>
+              <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+    
+          </div>
+      </Layout>
+    </>
+  );
+}
