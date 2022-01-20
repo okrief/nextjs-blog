e6003399e2ee0d9e1d8d29abaa2c202bf828ee08
Odelya <okrief@lightricks.com>
2022-01-20 16:32:49 +0200

    final-steps

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
+const name = 'Josaphine Terner'
 export const siteTitle = 'Josaphin\'s Blog'
 
 export default function Layout({ children, home }) {
---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import utilStyles from "../../styles/utils.module.css";
+import Layout from "../../components/layout";
+import Head from "next/head";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <dive className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </dive>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: { postData },
+  };
+}
